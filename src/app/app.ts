50a4a557ab5e3ac9983d0ae69718e01772368d58
Abdulakeem Bolarinwa <bolarinwa_akeem@yahoo.com>
2025-07-07 16:09:35 +0100

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../app/components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, NavbarComponent, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
